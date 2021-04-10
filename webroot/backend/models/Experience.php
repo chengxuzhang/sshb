89bf1b508ebad5b160a3934cd7bbdfe50aa6f48d
chengxuzhang <1066443079@qq.com>
2021-04-10 17:07:33 +0800

    25

--- webroot/backend/models/Experience.php
+++ webroot/backend/models/Experience.php
@@ -10,18 +10,14 @@ use Yii;
  * @property string $id
  * @property string $name
  * @property string $phone
- * @property string $type
- * @property string $province
- * @property string $city
- * @property integer $createTime
+ * @property string $title
+ * @property string $email
+ * @property string $content
+ * @property integer $create_time
  * @property integer $status
  */
 class Experience extends \backend\components\BaseModel
 {
-    public $statusParams = [
-        '10' => '已处理',
-        '20' => '未处理',
-    ];
     /**
      * @inheritdoc
      */
@@ -36,11 +32,11 @@ class Experience extends \backend\components\BaseModel
     public function rules()
     {
         return [
-            [['createTime', 'status'], 'integer'],
+            [['create_time', 'status'], 'integer'],
             [['name'], 'string', 'max' => 60],
             [['phone'], 'string', 'max' => 20],
-            [['type'], 'string', 'max' => 10],
-            [['province', 'city'], 'string', 'max' => 30],
+            [['title'], 'string', 'max' => 255],
+            [['email', 'content'], 'string', 'max' => 30],
         ];
     }
 
@@ -53,10 +49,10 @@ class Experience extends \backend\components\BaseModel
             'id' => 'ID',
             'name' => '姓名',
             'phone' => '电话',
-            'type' => '来源',
-            'province' => '省份',
-            'city' => '城市',
-            'createTime' => '申请日期',
+            'title' => '主题',
+            'email' => '邮箱',
+            'content' => '留言内容',
+            'create_time' => '留言时间',
             'status' => '状态',
         ];
     }
